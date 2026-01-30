bfbf7e93cc5ab1f60cb926f8a175269dcef5a387
Ahme-d711 <elgedawyahmed333@gmail.com>
2026-01-30 23:37:19 +0200

    feat: add CategorySection component and integrate it into HomeTemplate to display product categories.

--- client/features/main/home/templates/HomeTemplate.tsx
+++ client/features/main/home/templates/HomeTemplate.tsx
@@ -1,18 +1,23 @@
 "use client";
 
 import { MainNavbar } from "@/components/MainNavbar";
+import { useHomeAds, useHomeCategories } from "../hooks/useHome";
 import { HeroSection } from "../components/HeroSection";
-import { useHomeAds } from "../hooks/useHome";
+import CategorySection from "@/features/home/components/CategorySection";
 
 export default function HomeTemplate() {
   const { data: ads = [] } = useHomeAds();
+  const { data: categories = [] } = useHomeCategories();
 
   return (
     <div className="flex flex-col min-h-screen">
       <MainNavbar />
       
-        {/* Hero Section */}
-        <HeroSection ads={ads} />
+      {/* Hero Section */}
+      <HeroSection ads={ads} />
+
+      {/* Categories Section */}
+      <CategorySection categories={categories} />
     </div>
   );
 }
